ca093738c1aa282998203f7458b51bc5dfc31f61
tangyang <tangyang@sogou-inc.com>
2017-09-12 12:05:52 +0800

    webpack merge

--- bundler.js
+++ bundler.js
@@ -1,10 +1,26 @@
 const webpack = require('webpack');
 var webpackConfig = require('./webpack.dev.config.js');
+const merge = require('webpack-merge');
+
+var webpackConfig = merge({
+    output : {
+        publicPath: 'www.tangyang.com'
+    },
+    resolve : {
+        extensions : ['.css'],
+        alias : {
+            'src' : './src'
+        }
+    },
+},webpackConfig)
+
+console.log(webpackConfig);
 
 webpack(webpackConfig,function(err,stats) {
-    console.log(stats.toString({
+    process.stdout.write(stats.toString({
         colors : true,
         modules: true,
-        errors: true
+        errors: true,
+        publicPath : true
     }))
 });
